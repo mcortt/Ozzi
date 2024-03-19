a36a96db688cd95458f8bc845f7a7440fb86ea2c
Matthew Cody Ortt <9371806+mcortt@users.noreply.github.com>
2024-03-18 23:50:34 -0400

    Update 1.0.2
    fixed a couple of bugs
    added ipv6 support

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
 function identifyInput(input) {
     const ipPattern = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+    const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$/;
     const hashPattern = /^([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})$/;
     const portPattern = /^([0-9]{1,5})$/;
     const urlPattern = /^(https?:\/\/)?((?:[0-9]{1,3}\.){3}[0-9]{1,3}|[\da-z\.-]+)(:[0-9]{1,5})?([\/\w\.-]*)*\/?$/;
 
     if (ipPattern.test(input)) {
         searchIP(input);
+    } else if (ipv6Pattern.test(input)) {
+        searchIPv6(input);
     } else if (hashPattern.test(input)) {
         searchHash(input);
     } else if (portPattern.test(input)) {
@@ -45,6 +48,15 @@ function createTab(url) {
         active: false,
         url: url
     });
+}
+
+function searchIPv6(value) {
+    if (value) {
+        createTab('https://scamalytics.com/ip/' + value);
+        createTab('https://www.abuseipdb.com/check/' + value);
+        createTab('https://exchange.xforce.ibmcloud.com/ip/' + value);
+        createTab('https://isc.sans.edu/ipinfo/' + value);
+    }
 }
 
 function searchIP(value) {
